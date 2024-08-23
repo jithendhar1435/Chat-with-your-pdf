2ddfae432db868e7f1b443592bb8429a06718c65
coderjithu <92565190+jithendhar1435@users.noreply.github.com>
2024-08-24 00:44:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import os
 import google.generativeai as genai
 
 
-genai.configure(api_key="AIzaSyD9alsaje_ePtrdQZOU2zLb70coC755MTU")
+genai.configure(api_key="YOUR_API_KEY")
+#replace YOUR_API_KEY with your google gemini api key
 model = genai.GenerativeModel(model_name="gemini-1.5-flash")
 
 def extract_text_from_pdfs(pdf_files):
