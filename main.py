6d3d1f3974bf30288981f267c3e7ee69942761e1
coderjithu <92565190+jithendhar1435@users.noreply.github.com>
2024-08-24 21:27:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import PyPDF2
 import google.generativeai as genai
 
 
-genai.configure(api_key="YOUR_API_KEY")
+genai.configure(api_key="AIzaSyD9alsaje_ePtrdQZOU2zLb70coC755MTU")
 #replace YOUR_API_KEY with your google gemini api key
 model = genai.GenerativeModel(model_name="gemini-1.5-flash")
 
